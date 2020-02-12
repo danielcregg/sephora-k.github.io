ee489f401b0359b1fc1abb842d494228a6f0a489
sephora-k <g00362884@gmit.ie>
2020-02-12 16:39:15 +0000

    created webpages

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@
             <!-- Navbar content -->
             <ul class="nav">
             <li class="nav-item">
-                <a class="nav-link active" href="index.php">Home</a>
+                <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="gallery.php">Gallery</a>
@@ -20,25 +20,16 @@
                 <a class="nav-link" href="blog.php">Blog</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="contact.php">Contact</a>
+                <a class="nav-link active" href="contact.php">Contact</a>
             </li>
             </ul>
             </nav>
-
-            <p id=demo>Hello World!</p>
-            <script type="text/javascript" src="myscripts.js"></script>
-            <button onclick="myFunction();">Click Me to turn text blue</button>
-            
-            <div class="jumbotron">
-            <h1 class="display-4">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4">
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-            </div>
-
-
-        
+       
+       
+       
+       
+       
+       
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
