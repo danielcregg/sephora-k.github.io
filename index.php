ad389537607c83d40acbebf9a9126b1022cca2a6
sephora-k <g00362884@gmit.ie>
2020-02-25 23:26:46 +0000

    navbar changed

--- index.php
+++ index.php
@@ -5,10 +5,38 @@
         
     </head>
     <body>
-            <h1>My Website</h1>
+            
 
+        <nav class="navbar navbar-expand-md navbar dark bg-dark sticky-top"> <!-- (option)change sticky top to fixed bottom -->
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="Screenshot/logo.jpg"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav nl-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </nav>
+
+
+
+<!-- <h1>My Website</h1> 
             <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-            <!-- Navbar content -->
+             *Navbar content* 
             <ul class="nav">
             <li class="nav-item">
                 <a class="nav-link active" href="index.php">Home</a>
@@ -23,18 +51,32 @@
                 <a class="nav-link" href="contact.php">Contact</a>
             </li>
             </ul>
-            </nav>
+            </nav> -->
 
-            <p id=demo>Hello World!</p>
+          <!--  <p id=demo>Hello World!</p>
             <script type="text/javascript" src="myscripts.js"></script>
             <button onclick="myFunction();">Click Me to turn text blue</button>
+            -->
             
-            <div class="jumbotron">
-            <h1 class="display-4">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4">
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <!--  *CAROUSEL*  -->
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                 <img src="Screenshots/look_1.jpg" class="d-block w-100" alt="First Look">
+                </div>
+                <div class="carousel-item">
+                <img src="Screenshots/look_2.jpg" class="d-block w-100" alt="Second Look">
+                </div>
+                <div class="carousel-item">
+                <img src="Screenshots/look_3.jpg" class="d-block w-100" alt="Third Look">
+                </div>
+                <div class="carousel-item">
+                <img src="Screenshots/look_4.jpg" class="d-block w-100" alt="Fourth Look">
+                </div>
+                <div class="carousel-item">
+                <img src="Screenshots/look_5.jpg" class="d-block w-100" alt="Fifth Look">
+                </div>
+            </div>
             </div>
 
 
