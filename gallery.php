7a9683a96c82fd26b6aaa04b64552f664e47df72
sephora-k <g00362884@gmit.ie>
2020-03-01 23:13:58 +0000

    creating img gallery

--- gallery.php
+++ gallery.php
@@ -45,12 +45,22 @@
         </section>  
 
         <section class="fwh-slide fwh-slide--bg-silver">
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <div class="containerGallery">
+            <div class="box a"></div>
+            <div class="box b"></div>
+            <div class="box c"></div>
+            <div class="box d"></div>
+            <div class="box e"></div>
+            <div class="box f"></div>
+            <div class="box g"></div>
+            <div class="box h"></div>
+            <div class="box i"></div>
+            <div class="box j"></div>
+        </div>
+
+
+
+
         </section>   
        
        
