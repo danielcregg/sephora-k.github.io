2ec872cc958cad7af38c71af91401fb8a20dbac4
sephora-k <g00362884@gmit.ie>
2020-03-01 19:36:32 +0000

    images

--- index.php
+++ index.php
@@ -35,12 +35,15 @@
 <!-- *NAVBAR* -->
 
         <section class="fwh-slide">
+        <div class="container">
+        <img src="images/logo/logoAnimatedG.gif" class="img-fluid" alt="Simply Steph">
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
+        </div>
         </section>
 
         <section class="fwh-slide fwh-slide--bg-gray">
